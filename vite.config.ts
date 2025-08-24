2ec4c5e831aae9d43e557c09e92df243ebd03fca
Harry Posner <harryposner@users.noreply.github.com>
2025-08-23 21:26:20 -0700

    Remove Vite plugin for generating statement code

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { statementsPlugin } from './vite-plugin-statements'
 
 export default defineConfig({
-  plugins: [react(), statementsPlugin()],
+  plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
